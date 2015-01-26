db17c5785a7ca951a98a960b4391f645cbbe0496
Johannes Eickhold <jeick@eclipsesource.com>
2015-01-26 10:58:49 +0100

    Add support for screen rotation

--- device-info.js
+++ device-info.js
@@ -7,6 +7,8 @@ var page = tabris.create("Page", {
   topLevel: true
 });
 
+var screen = tabris.create("Composite", {background: "#fff", layoutData: {centerX: 0, top: [10, 0], }}).appendTo(page);
+
 var screenHeader = tabris.create("Label", {
   font: "24px",
   layoutData: {centerX: 0, top: sectionTopGap}
@@ -28,6 +30,30 @@ var platform = createKeyValue(model, "Platform:", device.platform);
 var version = createKeyValue(platform, "Version:", device.version);
 var language = createKeyValue(version, "Languag:", device.language);
 
+page.on("change:bounds", function(event) {
+  var bounds = this.get("bounds");
+  if(bounds.width > bounds.height) {
+    screenHeader.set("layoutData", {top: sectionTopGap, right: [80, 0]});
+    x.set("layoutData", {right: [80, 0], top: [screenHeader, 12]});
+    y.set("layoutData", {right: [80, 0], top: [x, 12]});
+    density.set("layoutData", {right: [80, 0], top: [y, 12]});
+    deviceHeader.set("layoutData", {top: sectionTopGap, right: [30, 0]});
+    model.set("layoutData", {right: [30, 0], top: [deviceHeader, 12]});
+    platform.set("layoutData", {right: [30, 0], top: [model, 12]});
+    version.set("layoutData", {right: [30, 0], top: [platform, 12]});
+    language.set("layoutData", {right: [30, 0], top: [version, 12]});
+  } else {
+    screenHeader.set("layoutData", {centerX: 0, top: sectionTopGap});
+    x.set("layoutData", {right: [70, 0], top: [screenHeader, 12]});
+    y.set("layoutData", {right: [70, 0], top: [x, 12]});
+    density.set("layoutData", {right: [70, 0], top: [y, 12]});
+    deviceHeader.set("layoutData", {centerX: 0, top: [density, sectionTopGap]});
+    model.set("layoutData", {left: 18, top: [deviceHeader, 12], right: [70, 0]});
+    platform.set("layoutData", {left: 18, top: [model, 12], right: [70, 0]});
+    version.set("layoutData", {left: 18, top: [platform, 12], right: [70, 0]});
+    language.set("layoutData", {left: 18, top: [version, 12], right: [70, 0]});
+  }
+});
 page.open();
 
 function createKeyValue(previous, key, value) {
